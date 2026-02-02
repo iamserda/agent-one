13055905a026a0b05f3f30d242631bfe8e19624b
iamserda <12580399+iamserda@users.noreply.github.com>
2026-02-01 22:59:38 -0500

    feat: added --verbose flag to reduce the output to only when the user wants to see all the info. --@iamserda

--- main.py
+++ main.py
@@ -2,27 +2,31 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-gemini_client = genai.Client(api_key=api_key)
 
 
-def main():
+def main(api_key: str):
+    gemini_client = genai.Client(api_key=api_key)
     if api_key is None:
         raise RuntimeError("Please check your secrets' source file. A key with the value provided was not found.")
+
     parser = argparse.ArgumentParser(
         prog="gemini-client",
         usage="Enter your prompt here: ",
         description="Takes your question or prompt, and returns Gemini 2.5 Flash's response to the prompt",
     )
     parser.add_argument("user_prompt", type=str, help="Enter your prompt or questions for Gemini AI here: ")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     _prompt = args.user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = gemini_client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
+
     if response is None or response.usage_metadata is None:
         raise RuntimeError("No response was provide. Please check configuration and connection and try again!")
 
@@ -31,14 +35,17 @@ def main():
         response.usage_metadata.candidates_token_count,
         response.text,
     )
-
-    results = f"""
+    response = f"Response: {gemini_response}"
+    verbose_response = f"""
 User prompt: {_prompt}
 Prompt tokens: {req_token_count}
 Response tokens: {res_token_count}
-Response:
-{gemini_response}"""
-    print(results)
+{response}"""
+    if args.verbose:
+        print(verbose_response)
+    else:
+        print(response)
 
 if __name__ == "__main__":
-    main()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    main(api_key=api_key)
