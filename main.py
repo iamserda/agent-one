66ec2eb32c205347930bc89590a5f65af53eb299
iamserda <12580399+iamserda@users.noreply.github.com>
2026-02-16 01:06:17 -0500

    feat: added calculator app functionality which will be the app our code-agent will work on and added unit tests for various operations for the calculator app --@iamserda

--- main.py
+++ main.py
@@ -5,38 +5,48 @@ from google import genai
 from google.genai import types
 
 load_dotenv()
+API_KEY: str | None = os.environ.get("GEMINI_API_KEY")
 
 
-def main(api_key: str):
-    gemini_client = genai.Client(api_key=api_key)
-    if api_key is None:
-        raise RuntimeError("Please check your secrets' source file. A key with the value provided was not found.")
-
+def main():
+    if API_KEY is None:
+        raise RuntimeError(
+            "Please check your secrets' source file. A key with the value provided was not found."
+        )
+    gemini_client = genai.Client(api_key=API_KEY)
     parser = argparse.ArgumentParser(
         prog="gemini-client",
         usage="Enter your prompt here: ",
         description="Takes your question or prompt, and returns Gemini 2.5 Flash's response to the prompt",
     )
-    parser.add_argument("user_prompt", type=str, help="Enter your prompt or questions for Gemini AI here: ")
+    parser.add_argument(
+        "user_prompt",
+        type=str,
+        help="Enter your prompt or questions for Gemini AI here: ",
+    )
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     _prompt = args.user_prompt
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = gemini_client.models.generate_content(
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+    response: genai.GenerateContentResponse = gemini_client.models.generate_content(  # type: ignore
         model="gemini-2.5-flash",
         contents=messages,
     )
 
-    if response is None or response.usage_metadata is None:
-        raise RuntimeError("No response was provide. Please check configuration and connection and try again!")
+    if response is None or response.usage_metadata is None:  # type: ignore
+        raise RuntimeError(
+            "No response was provide. Please check configuration and connection and try again!"
+        )
 
-    req_token_count, res_token_count, gemini_response = (
-        response.usage_metadata.prompt_token_count,
-        response.usage_metadata.candidates_token_count,
-        response.text,
+    req_token_count, res_token_count, gemini_response = (  # type: ignore
+        response.usage_metadata.prompt_token_count,  # type: ignore
+        response.usage_metadata.candidates_token_count,  # type: ignore
+        response.text,  # type: ignore
     )
-    response = f"Response: {gemini_response}"
-    verbose_response = f"""
+    response: str = f"Response: {gemini_response}"
+    verbose_response: str = f"""
 User prompt: {_prompt}
 Prompt tokens: {req_token_count}
 Response tokens: {res_token_count}
@@ -46,6 +56,6 @@ Response tokens: {res_token_count}
     else:
         print(response)
 
+
 if __name__ == "__main__":
-    api_key = os.environ.get("GEMINI_API_KEY")
-    main(api_key=api_key)
+    main()
